f6fe334422338625259af8e3ec30d1b56196ec12
v1nl <219304@student.pwr.edu.pl>
2015-06-01 17:51:55 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include "instrument.h"
+#include "gitara.h"
+
+using namespace std;
+
+int main()
+
+{
+
+
+Gitara gitara1;
+	
+	while(true)
+{
+	gitara1.Menu();
+
+	
+
+}
+
+	return 0;
+
+}
